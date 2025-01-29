f164d27bdcd7f05124203eada69af4a4a587a8cd
thetamannakhurana <115238314+thetamannakhurana@users.noreply.github.com>
2025-01-30 02:00:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     var password = document.getElementById("password").value;
-    var correctPassword = "10022023cutie"; // Change this
+    var correctPassword = "mycutie"; // Change this
 
     if (password === correctPassword) {
         window.location.href = "secret.html"; 
